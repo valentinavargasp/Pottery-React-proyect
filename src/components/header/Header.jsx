5053d470aae384a6c262ef26a6a8ae32a55bced2
Valentina <valentinapescarav@gmail.com>
2024-06-30 19:22:55 -0300

    bootstrap+fontface

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,16 +1,34 @@
-import React from 'react'
-import { NavBar } from './NavBar'
-import logo from '../../assets/logo.svg'
-import { CartWidget } from './CartWidget'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { NavBar } from './NavBar';
+import { Link } from 'react-router-dom';
+import logo from '../../assets/logo.svg';
+import { CartWidget } from './CartWidget';
 
 export const Header = () => {
-
   return (
     <header className='header'>
-      <Link to="/"> <img className='logoImg' src={logo} alt="" /> </Link>
-      <NavBar />
-      <CartWidget />
+      <nav className="navbar navbar-expand-lg">
+        <div className="container-fluid d-flex align-items-center justify-content-between">
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <Link to="/" className="navbar-brand mx-auto">
+            <img src={logo} alt="Logo" className='logoImg' />
+          </Link>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <NavBar />
+          </div>
+          <div className="cart-widget">
+            <CartWidget />
+          </div>
+        </div>
+      </nav>
     </header>
-  )
+  );
 }
+
+
+
+
+
+
--- src/components/header/NavBar.jsx
+++ src/components/header/NavBar.jsx
@@ -1,38 +1,34 @@
 import React, { useEffect, useState } from 'react';
-import { Link, NavLink } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { collection, getDocs } from "firebase/firestore";
 import { db } from "../../firebase/config";
 
 export const NavBar = () => {
-
-  let [categories, setCategories] = useState([]);
+  const [categories, setCategories] = useState([]);
 
   useEffect(() => {
     const categoriasRef = collection(db, "categorias");
     getDocs(categoriasRef)
       .then((res) => {
         setCategories(res.docs.map((doc) => {
-          return { ...doc.data() }
+          return { ...doc.data(), id: doc.id };
         }));
-      })
-  }, [])
+      });
+  }, []);
 
   return (
-    <nav>
-      <ul className='menuNav'>
-        <li> <NavLink to="/" activeclassname="active" className="navLink"> Inicio </NavLink> </li>
-        {
-          categories.map((category) => {
-            return (
-              <li key={category.id}>
-                <NavLink to={`/category/${category.id}`} activeclassname="active" className="navLink" >
-                  {category.nombre}
-                </NavLink>
-              </li>
-            )
-          })
-        }
-      </ul>
-    </nav>
-  )
+    <ul className="navbar-nav">
+      <li className="nav-item">
+        <NavLink to="/" activeClassName="active" className="navLink nav-link" aria-current="page">Inicio</NavLink>
+      </li>
+      {categories.map((category) => (
+        <li key={category.id} className="nav-item">
+          <NavLink to={`/category/${category.id}`} activeClassName="active" className="navLink nav-link">
+            {category.nombre}
+          </NavLink>
+        </li>
+      ))}
+    </ul>
+  );
 }
+
